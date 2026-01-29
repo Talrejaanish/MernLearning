56b1c1c38ff78fa2cc6320839d6a4ee8064c6759
Talrejaanish <talrejaanish1@gmail.com>
2026-01-29 22:45:20 +0530

    loops,switch,conditional statement in js

--- javascript/sript.js
+++ javascript/sript.js
@@ -1,2 +1,15 @@
 const name = "John Doe";
 console.log("Hello, " + name + "!");
+let a=7;
+let b=10;
+let c=a+b;
+console.log("addition =",c);
+let d=null;
+console.log(typeof(c));
+let e;
+console.log(typeof(e));
+if(a>b){
+    console.log("a is greater than b");
+}else{
+    console.log("b is greater than a");
+}
---
+++ javascript/switch.js
@@ -0,0 +1,27 @@
+const av=50;
+switch(av){
+    case 10:
+        console.log("Value is 10");
+        break;
+    case 20:
+        console.log("Value is 20");
+        break;
+    case 30:
+        console.log("Value is 30");
+        break;
+    default:
+        console.log("Value is not 10, 20 or 30");
+}
+let x=0;
+while(x<5){
+    console.log(x);
+    x++;
+}
+for(let i=0;i<5;i++){
+    console.log("i =",i);
+}
+x1 =0;
+do{
+    console.log("x in do jjwhile =",x1);
+    x1++;
+}while(x1<10);
