d5b4010562b1271af596bf65b0d3474c4158cee0
Talrejaanish <talrejaanish1@gmail.com>
2026-02-02 00:50:47 +0530

    Dom manipulation

---
+++ javascript/Dom manipulation/script.js
@@ -0,0 +1,16 @@
+// console.log(document.body);
+const bodybg = document.body;
+// bodybg.style.backgroundColor = "green";
+// const box4=document.getElementById("box4");
+// box4.textContent="Hello Anish";
+// // box4.style.backgroundColor="white";
+// const div =document.getElementById('div');
+// console.log(div);
+// const box=document.getElementsByClassName("box");
+// console.log(box);
+// box.classList.add("circle");
+
+const p=document.createElement("p");
+p.innerHTML="This is a paragraph added by JS";
+p.classList.add("para");
+cont.appendChild(p);
